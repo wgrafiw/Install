90d03230769ec97d5a4467d57f4b262339082d74
wgrafiw <99220377+wgrafiw@users.noreply.github.com>
2022-08-28 10:27:20 -0300

    Update InstalFull.sh

--- InstalFull.sh
+++ InstalFull.sh
@@ -1,11 +1,11 @@
-echo "Instalar Ngrok"
+apt-get install curl
+apt-get install unzip
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
 unzip ngrok-stable-linux-amd64.zip > /dev/null 2>&1
 ./ngrok authtoken 1uGjsrrybf6VNQSBvHMeVIEYijd_5e2Y7ZLpGKGJo5c1Cqhvd 
 nohup ./ngrok tcp 22 &>/dev/null &
 echo Tải xuống tệp từ sever
 echo Tải xuống tệp từ sever
-apt-get install qemu
 echo "Đợi"
 echo "Khởi động Windows"
 echo RDP Địa chỉ:
@@ -15,5 +15,6 @@ echo "Đợi 1-2 Phút để hoàn thành thiết lập"
 echo "Không đóng tab này"
 echo "hãy ủng hộ Hoàng vtmc xin cảm ơn"
 apt-get install curl
-curl https://rclone.org/install.sh | sudo bash
+curl https://rclone.org/install.sh
+bash install.sh
 apt-get install fuse
